5654a56642808b94a81e6848125f5b0aafe2ffbc
GUILHERME FERRO <guilherme.ferro@fiocruz.br>
2021-11-11 10:10:53 -0300

    ajustando para nome composto

--- src/Commands/TemplateGenerateEasyCommand.php
+++ src/Commands/TemplateGenerateEasyCommand.php
@@ -4,27 +4,28 @@ namespace Gsferro\TemplateGenerateEasy\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\File;
-use Illuminate\Support\Str;
+use function Livewire\str;
 
 class TemplateGenerateEasyCommand extends Command
 {
     private string $entite;
-    private bool   $createDash = false;
-    private bool   $createModal = false;
+    private bool   $createDash   = false;
+    private bool   $createModal  = false;
+    private bool   $createImport = false;
 
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected string $signature = 'gsferro:template-generate {entite : Entite name} '; # {--fields=}
+    protected $signature = 'gsferro:template-generate {entite : Entite name} '; # {--fields=}
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected string $description = 'Generate all files for new Entite!';
+    protected $description = 'Generate all files for new Entite!';
 
     /**
      * Create a new command instance.
@@ -65,53 +66,94 @@ class TemplateGenerateEasyCommand extends Command
         |       - criar a pasta da entidade com o form pegando os fields
         |
         */
-        $this->call("make:model",     ["name" => "{$this->entite}", "-m" => "-m"]);  # ser executada via blueprint
-        $this->call("make:datatable", ["name" => "{$this->entite}/Table", "model" => "{$this->entite}"]);
-        $this->call("livewire:make",  ["name" => "{$this->entite}/Index",  "--test" => "--test", "--stub" => "vendor/gsferro/template-generate-easy/src/stubs/livewire/index"]); # com o stub modificado
-        $this->call("livewire:make",  ["name" => "{$this->entite}/Create", "--test" => "--test", "--stub" => "vendor/gsferro/template-generate-easy/src/stubs/livewire/create"]); # com o stub modificado # todo unificar
-        $this->call("livewire:make",  ["name" => "{$this->entite}/Edit",   "--test" => "--test", "--stub" => "vendor/gsferro/template-generate-easy/src/stubs/livewire/edit"]); # todo unificar
-        $this->call("pest:test",      ["name" => "{$this->entite}ModelTest", "--unit" => "--unit"]); # test model # Todo criar stub
-        $this->call("dusk:make",      ["name" => "{$this->entite}DuskTest"]); # Todo criar stub
 
-        /*
-        |---------------------------------------------------
-        | alterar os arquivos apos a criação colocando a Model
-        |---------------------------------------------------
-        */
-        $this->applyModelInView();
-        $this->applyModelInClass();
+        try {
+            $this->call("make:model", [
+                "name" => "{$this->entite}",
+                "-m"   => "-m"
+            ]); # ser executada via blueprint
 
-        /*
-        |---------------------------------------------------
-        | criar dashboard
-        |---------------------------------------------------
-        |
-        | TODO
-        */
-        if ($this->createDash) {
-//            $this->call("livewire:make",    ["name" => "Dashboard/{$this->entite}/Edit",   "--test" => "--test", "--stub" => "vendor/gsferro/template-generate-easy/src/stubs/livewire/dashboard"]);
-        }
+            $this->call("make:datatable", [
+                "name"  => "{$this->entite}/Table",
+                "model" => "{$this->entite}"
+            ]);
 
-        /*
-        |---------------------------------------------------
-        | criar modal
-        |---------------------------------------------------
-        |
-        | TODO
-        */
-        if ($this->createModal) {
-//            $this->call("livewire:make",    ["name" => "{$this->entite}/Modal", "--test" => "--test", "--stub" => "vendor/gsferro/template-generate-easy/src/stubs/livewire/modal"]);
+            $this->call("livewire:make", [
+                "name"   => "{$this->entite}/Index",
+                "--test" => "--test",
+                "--stub" => "vendor/gsferro/template-generate-easy/src/stubs/livewire/index"
+            ]); # com o stub modificado
+
+            $this->call("livewire:make", [
+                "name"   => "{$this->entite}/Create",
+                "--test" => "--test",
+                "--stub" => "vendor/gsferro/template-generate-easy/src/stubs/livewire/create"
+            ]); # com o stub modificado # todo unificar
+
+            $this->call("livewire:make", [
+                "name"   => "{$this->entite}/Edit",
+                "--test" => "--test",
+                "--stub" => "vendor/gsferro/template-generate-easy/src/stubs/livewire/edit"
+            ]); # todo unificar
+
+            $this->call("pest:test", [
+                "name"   => "{$this->entite}ModelTest",
+                "--unit" => "--unit"
+            ]); # test model # Todo criar stub
+
+            $this->call("dusk:make", [
+                "name" => "{$this->entite}DuskTest"
+            ]); # Todo criar stub
+
+            /*
+            |---------------------------------------------------
+            | alterar os arquivos apos a criação colocando a Model
+            |---------------------------------------------------
+            */
+            $this->applyModelInView();
+            $this->applyModelInClass();
+
+            /*
+            |---------------------------------------------------
+            | criar import
+            |---------------------------------------------------
+            */
+            if ($this->createImport) {
+                // todo
+            }
+
+            /*
+            |---------------------------------------------------
+            | criar dashboard
+            |---------------------------------------------------
+            */
+            if ($this->createDash) {
+                //            $this->call("livewire:make",    ["name" => "Dashboard/{$this->entite}/Edit",   "--test" => "--test", "--stub" => "vendor/gsferro/template-generate-easy/src/stubs/livewire/dashboard"]);
+            }
+
+            /*
+            |---------------------------------------------------
+            | criar modal
+            |---------------------------------------------------
+            */
+            if ($this->createModal) {
+                //            $this->call("livewire:make",    ["name" => "{$this->entite}/Modal", "--test" => "--test", "--stub" => "vendor/gsferro/template-generate-easy/src/stubs/livewire/modal"]);
+            }
+
+        } catch (\Exception $e) {
+            dump('Ops:', $e->getMessage());
         }
     }
 
     private function applyModelInView()
     {
-        $pathViews = resource_path('views');
+        $pathViews = resource_path('views/livewire');
 
         $acoes = ["index", "create", "edit"];
-        foreach ($acoes as $view) {
-            $paht = "{$pathViews}\livewire\\".Str::snake($this->entite)."\\{$view}.blade.php";
-            File::put($paht, $this->applyGrid($paht));
+        foreach ($acoes as $file) {
+            $file = "{$pathViews}\\".str($this->entite)->kebab()."\\$file.blade.php";
+
+            $this->applyInClass($file);
         }
     }
 
@@ -121,16 +163,25 @@ class TemplateGenerateEasyCommand extends Command
 
         $acoes = ["Index", "Create", "Edit"];
         foreach ($acoes as $file) {
-            $paht = "{$pathLivewire}\\$this->entite\\$file.php";
-            File::put($paht, $this->applyGrid($paht));
+            $file = "{$pathLivewire}\\$this->entite\\$file.php";
+
+            $this->applyInClass($file);
         }
     }
 
-    private function applyGrid($file)
+    private function applyInClass($file)
     {
+        return File::exists($file) ? File::put($file, $this->applyReplace($file)) : false;
+    }
+
+    private function applyReplace($file)
+    {
+        $search  = ['/\[Model\]/', /*'/\[model\]/', */'/\{Model\}/', '/\[pathLivewire\]/'];
+        $replace = [$this->entite, /*strtolower($this->entite),*/ $this->entite, str($this->entite)->kebab()];
+
         return preg_replace(
-            ['/\[Model\]/', '/\[model\]/','/\{Model\}/', '/\[pathLivewire\]/' ],
-            [$this->entite, strtolower($this->entite), $this->entite, Str::snake($this->entite) ],
+            $search,
+            $replace,
             file_get_contents("{$file}")
         );
     }
